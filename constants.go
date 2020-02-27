abb4c4e050860e989464c14146436aa04b95c5ef
Praveen Kumar <kumarpraveen.nitdgp@gmail.com>
2020-02-27 15:01:15 +0530

    cut 0.12.7

--- constants.go
+++ constants.go
@@ -2,7 +2,7 @@ package libvirt
 
 const (
 	DriverName    = "libvirt"
-	DriverVersion = "0.12.6"
+	DriverVersion = "0.12.7"
 
 	connectionString = "qemu:///system"
 	dnsmasqLeases    = "/var/lib/libvirt/dnsmasq/%s.leases"
